53a85255e95fa82250049fd554e6342f5b00a94c
cmerchant <@cmerchant>
2018-12-21 16:33:09 -0500

    adding bootstrap

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,4 @@ class Post < ApplicationRecord
 	belongs_to :category
 	validates :title, :content, :category_id, presence: true
 	has_many :line_items, inverse_of: :order
-	<%= render ‘shared/error_messages’ %>
-
 end
