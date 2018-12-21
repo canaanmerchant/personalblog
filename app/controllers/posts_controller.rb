a19ad1c8677bd31f591a0176957613b689af4017
cmerchant <@cmerchant>
2018-12-21 15:49:42 -0500

    updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,30 +4,48 @@ class PostsController < ApplicationController
 
 	def index
 		@posts = Post.all
+		if params[:category].blank?
+  		@posts = Post.all.order("created_at DESC")
+  		else
+  		@category_id = Category.find_by(name: params[:category]).id
+  		@posts = Post.where(category_id: @category_id).order("created_at DESC")
+  		end
 	end
-
-
-
 
 	def show
 	end
 
-	def create
+	def new
+		@post = Post.new
 	end
 
 	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post, notice: "This post was created!"
+		else
+			render 'new'
+		end 
 	end
 
 	def edit
 	end
 
 	def update
+		if @post.update(post_params)
+          redirect_to @post, notice: "Update successful"
+     else
+          render 'edit'
+     end
 	end
 
 	def destroy
+		@post.destroy
+		redirect_to root_pat, notice: "Post destroyed!"
 	end
 
 private 
+
 	def post_params
 		 params.require(:post).permit(:title, :content, :category_id)
 	end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
 	belongs_to :category
 	validates :title, :content, :category_id, presence: true
+	has_many :line_items, inverse_of: :order
+	<%= render ‘shared/error_messages’ %>
+
 end
